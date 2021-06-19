fad631b1be8df206f118b9076cdc0f078fd4ba6e
satwik singh <61342456+Satwik-Singh@users.noreply.github.com>
2021-06-19 12:40:32 +0530

    initial commit

---
+++ covid_fix/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container bg-primary text-center">
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
